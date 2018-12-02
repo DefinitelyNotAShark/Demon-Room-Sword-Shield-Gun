2cad8b7351416d0ddfea3197ace0be87976fe24e
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-12-02 14:54:27 -0600

    Added some scripts to deal with collision detection

---
+++ Assets/Scripts/DetectSwordCollision.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this goes on the sword and handles any collision with it
+
+public class DetectSwordCollision : MonoBehaviour
+{
+    private IFightable fightableObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        try
+        {
+            fightableObject = other.GetComponent<IFightable>();//try to see if the object is fightable
+            fightableObject.SwordHit();//do whatever it's supposed to if it's hit
+
+            Debug.Log("The sword hit an enemy");
+        }
+        catch(NullReferenceException)//if there's no fightable object
+        {
+            Debug.Log("The sword hit something other than an enemy");
+        }
+    }
+}
