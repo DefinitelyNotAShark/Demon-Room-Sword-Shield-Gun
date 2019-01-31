4e98d324c981fcf0ca62511b1b0eb150394bea5c
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-01-31 17:06:29 -0600

    Made a base class for Enemy
    Gives vars for all the Enemies. Shared vars like
    -Lives
    -isDead
    Should help with health system

---
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour, IFightable
+{
+    public bool EnemyIsDead;
+    public int EnemyLives;
+
+    public void SwordHit()
+    {
+        EnemyLives -= 2;
+    }
+
+    public void GunHit()
+    {
+        EnemyLives--;
+    }
+}
+
