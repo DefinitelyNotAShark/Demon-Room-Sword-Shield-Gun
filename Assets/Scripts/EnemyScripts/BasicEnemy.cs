19ff17089fa4629c9aa4a0247f1ce3b3f93afdae
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-02-15 17:25:33 -0600

    Got gun to work with collision

--- Assets/Scripts/EnemyScripts/BasicEnemy.cs
+++ Assets/Scripts/EnemyScripts/BasicEnemy.cs
@@ -16,10 +16,6 @@ public class BasicEnemy : Enemy
     [SerializeField]
     private int enemyLives;
 
-    private ParticleSystem particles;
-    private MeshRenderer mesh;
-    private BoxCollider collider;
-
 
     private bool coroutineStarted;
 
@@ -27,27 +23,11 @@ public class BasicEnemy : Enemy
     {
         //Vars
         EnemyLives = enemyLives;//set our base lives to our customized lives
-
-        //Components
-        particles = GetComponentInChildren<ParticleSystem>();
-        mesh = GetComponent<MeshRenderer>();
-        collider = GetComponent<BoxCollider>();
     }
 
     private void Update()
     {
         if (EnemyIsDead() && !coroutineStarted)//detects if enemy has been killed every frame
-            StartCoroutine(DestroyEnemyCooldown());
-    }
-
-    IEnumerator DestroyEnemyCooldown()
-    {
-        //AUDIO play enemy hit sound
-        coroutineStarted = true;
-        particles.Play();//give us some death particles!
-        mesh.enabled = false;//invisible
-        collider.enabled = false;//can't hit again
-        yield return new WaitForSeconds(enemyCoolDownLength);
-        Destroy(this.gameObject);//DIE
+            Destroy(this.gameObject);
     }
 }
