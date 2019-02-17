82e0d49c34963faf5138623c7ca725bfa8a84516
JWhittom <jawhittom@gmail.com>
2019-02-17 14:05:34 -0600

    Sword Swoosh
    Adds a trail to the sword prefab and a script which activates it when the sword moves fast enough.

---
+++ Assets/Scripts/SwordScripts/SwordSwishManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSwishManager : MonoBehaviour
+{
+    [SerializeField]
+    private float swooshVelocity = 5;
+
+    private TrailRenderer trailRenderer;
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        trailRenderer = GetComponentInChildren<TrailRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (rigidbody.velocity.magnitude >= swooshVelocity)
+        {
+            // Swoosh audio here
+            trailRenderer.emitting = true;
+        }
+        else
+            trailRenderer.emitting = false;
+	}
+}
