022a5e1a658eeb2d91e78bcc0f1ba914daad8414
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-02-22 15:13:42 -0600

    Added in new colloseum and animations

--- Assets/Scripts/GunScripts/SpawnBullet.cs
+++ Assets/Scripts/GunScripts/SpawnBullet.cs
@@ -17,6 +17,14 @@ public class SpawnBullet : MonoBehaviour
     [SerializeField]
     private float bulletLifeTime;
 
+    [SerializeField]
+    private Light muzzleFlashLight;
+
+    [SerializeField]
+    private float lightThreshold = .5f;
+
+    private float lastShoot;
+
     private ParticleSystem muzzleFlash;
 
     private Vector3 direction;
@@ -24,6 +32,7 @@ public class SpawnBullet : MonoBehaviour
     private void Start()
     {
         muzzleFlash = GetComponentInChildren<ParticleSystem>();
+        muzzleFlashLight.enabled = false;
     }
 
     private void Update()
@@ -31,11 +40,15 @@ public class SpawnBullet : MonoBehaviour
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
             //AUDIO play a shooting sound here
+            lastShoot = Time.time;
+            muzzleFlashLight.enabled = true;
             muzzleFlash.Play();
 
             GameObject bulletInstance = Instantiate(bulletPrefab, transform.position, transform.rotation);//instantiates a bullet at the spawn position and at it's set prefab rotation
             bulletInstance.AddComponent<MoveBullet>().bulletSpeed = bulletSpeed;//give us a move script and set the speed to our bulletSpeed
             bulletInstance.GetComponent<MoveBullet>().bulletLifeTime = bulletLifeTime;
         }
+        if (Time.time - lastShoot > lightThreshold)
+            muzzleFlashLight.enabled = false;
     }
 }
