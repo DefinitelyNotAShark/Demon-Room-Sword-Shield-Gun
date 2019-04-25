3bab4d633beb1c18bb18cc1552e912e7f451ca9b
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-04-25 15:21:05 -0500

    Getting rid of the ugly screen tint. I need to find a better way to do this. Comment still in so I know where to put a new one

--- Assets/Scripts/EnemyDetectColl.cs
+++ Assets/Scripts/EnemyDetectColl.cs
@@ -5,15 +5,11 @@ using UnityEngine.UI;
 
 public class EnemyDetectColl : MonoBehaviour
 {
-    private GameObject panel;
-    private TintScreen tint;
     private AudioSource audioSource;//this is the audio source that displays the melee enemy punch sound
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        panel = GameObject.FindGameObjectWithTag("Panel");
-        tint = panel.GetComponent<TintScreen>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,7 +20,7 @@ public class EnemyDetectColl : MonoBehaviour
             {
                 audioSource.Play();
             }
-            StartCoroutine(tint.FadeImage());
+            //TINT
         }
     }
 }
