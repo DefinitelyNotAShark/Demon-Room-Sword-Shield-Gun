8b803d6451526bc9c5bdc59ea3db18644bcbc880
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-04-25 13:45:51 -0500

    Added a charge gun class and edited the spawn bullet class to use it

---
+++ Assets/Scripts/WeaponScripts/ChargeGun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeGun : MonoBehaviour
+{
+    [Header("Gun Charging Values")]
+    [SerializeField]
+    private Slider gunChargingUI;
+
+    [SerializeField]
+    [Tooltip("The time it takes for the gun to recharge.")]
+    private float maxChargeTime;
+
+    [HideInInspector]
+    public bool gunIsCharged = false;
+
+    private float currentChargeTime;
+
+	void Start ()
+    {
+        currentChargeTime = maxChargeTime;
+	}
+	
+	void Update ()
+    {
+        //CHANGE THE CHARGE
+        if (currentChargeTime < maxChargeTime)//if it can be charged more, keep charging it
+            currentChargeTime += Time.deltaTime;
+
+        else gunIsCharged = true;//otherwise, it is charged
+
+        //DISPLAY THE CHARGE
+        gunChargingUI.value = currentChargeTime / maxChargeTime;
+	}
+
+    /// <summary>
+    /// Call this every time a charge on the gun is used
+    /// </summary>
+    public void ResetGunCharge()
+    {
+        currentChargeTime = 0;
+    }
+}
---
+++ Assets/Scripts/WeaponScripts/SpawnBullet.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBullet : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    [Tooltip("The time until another shot is allowed after the previous shot.")]
+    private float shootCoolDownTime;
+
+    [SerializeField]
+    private float bulletSpeed;
+
+    [SerializeField]
+    private float bulletLifeTime;
+
+    [SerializeField]
+    private Light muzzleFlashLight;
+
+    [SerializeField]
+    private float lightThreshold = .5f;
+
+    [SerializeField]
+    private AudioClip audioClip;
+
+    private float lastShoot;
+
+    private ParticleSystem muzzleFlash;
+    private Vector3 direction;
+    private AudioSource audioSource;
+
+    private ChargeGun gunCharge;//ref to script that handles the charging of the gun
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        muzzleFlash = GetComponentInChildren<ParticleSystem>();
+        gunCharge = GetComponentInParent<ChargeGun>();
+
+        muzzleFlashLight.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            if (gunCharge.gunIsCharged)//strong shoot
+            {
+                if (audioSource != null && audioClip != null)
+                {
+                    audioSource.clip = audioClip;
+                    audioSource.Play();
+                }
+
+                lastShoot = Time.time;
+                muzzleFlashLight.enabled = true;
+                muzzleFlash.Play();
+                ShootBullet();
+            }
+            else//weak shoot
+            {
+                //AUDIO play weak shot sound
+
+                ShootBullet();
+            }
+
+            gunCharge.ResetGunCharge();//Either way, reset the charge
+        }
+
+        if (Time.time - lastShoot > lightThreshold)
+            muzzleFlashLight.enabled = false;
+    }
+
+    //spawn the bullet and set its params to the ones set in the serialize fields of the class
+    void ShootBullet()
+    {
+        //INSTANTIATE BULLET
+        GameObject bulletInstance = Instantiate(bulletPrefab, transform.position, transform.rotation);//instantiates a bullet at the spawn position and at it's set prefab rotation
+        bulletInstance.AddComponent<MoveBullet>().bulletSpeed = bulletSpeed;//give us a move script and set the speed to our bulletSpeed
+        bulletInstance.GetComponent<MoveBullet>().bulletLifeTime = bulletLifeTime;
+    }
+}
